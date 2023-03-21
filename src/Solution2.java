4d1d37b61fd17fb898768f58fb7f5db33d65913d
calaf <117057567+ca1af@users.noreply.github.com>
2023-03-21 20:11:12 +0900

    init

---
+++ src/Solution2.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+// 배포는 무조건 순서대로 되야한다.
+// 진도는 각각 쭉쭉나간다.
+
+public class Solution2 {
+    public int[] solution(int[] progresses, int[] speeds) {
+
+
+        int[] answer = {};
+        return answer;
+    }
+}
