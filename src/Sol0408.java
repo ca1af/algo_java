cfaafe5bb0e3ae504e5bb8be088cac6b8e0951c9
calaf <117057567+ca1af@users.noreply.github.com>
2023-04-10 19:18:54 +0900

    0410 알고리즘 스타트

--- src/Sol0408.java
+++ src/Sol0408.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Sol0408 {
     public int solution2(int k, int d) {
@@ -46,7 +44,81 @@ public class Sol0408 {
     //https://school.programmers.co.kr/learn/courses/30/lessons/77884
 
     public int solution(int left, int right) {
+        int count = 0;
         int answer = 0;
+
+        for (int i = left; i <= right; i++) {
+            for (int j = 1; j <= right; j++) {
+                if (i % j == 0){
+                    count++;
+                }
+            }
+            if (count % 2 == 0){
+                answer += i;
+            } else answer -= i;
+
+            count = 0;
+        }
+
+        return answer;
+    }
+
+    //https://school.programmers.co.kr/learn/courses/30/lessons/147355
+
+    public int solution(String t, String p) {
+        int answer = 0;
+        for (int i = 0; i <= t.length() - p.length(); i++) {
+            if (Integer.parseInt(t.substring(i, p.length() + i)) <= Integer.parseInt(p)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public int solution2(String t, String p) {
+        int answer = 0;
+        long pValue = Long.parseLong(p);
+        for (int i = 0; i <= t.length() - p.length(); i++) {
+            String subString = t.substring(i, p.length() + i);
+            if (Long.parseLong(subString) < pValue) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    //https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=java
+
+    public Object[] solution1(int[] numbers) {
+        int[] answer = {};
+        Set<Integer> set = new TreeSet<>();
+
+        for (int number : numbers) {
+            for (int i : numbers) {
+                set.add(number + i);
+            }
+        }
+
+        Object[] objects = set.toArray();
+
+        return objects;
+    }
+
+    public int[] solution(int[] numbers) {
+        Set<Integer> set = new TreeSet<>();
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        int[] answer = new int[set.size()];
+        int idx = 0;
+        for (int num : set) {
+            answer[idx++] = num;
+        }
+
         return answer;
     }
 }
