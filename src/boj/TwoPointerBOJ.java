859b329e6c7c07e9317672e2dfa03ca55ca088d0
DK <laancer4@gmail.com>
2023-05-31 01:53:32 +0900

    BOJ Two pointer

--- src/boj/TwoPointerBOJ.java
+++ src/boj/TwoPointerBOJ.java
@@ -69,45 +69,47 @@ public class TwoPointerBOJ {
 
             System.out.println(answer);
         }
-    }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+            int N = Integer.parseInt(br.readLine());
+            int M = Integer.parseInt(br.readLine());
+            int[] arr = new int[N];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arr);
-
-        int i=0, j=arr.length-1;
-        int cnt = 0;
-
-        while(i < j) {
-            if(arr[i] + arr[j] == M) {
-                i++;
-                j--;
-                cnt++;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int i=0; i<N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
             }
-            else {
-                if(arr[i] + arr[j] > M) {
+
+            Arrays.sort(arr);
+
+            int i=0, j=arr.length-1;
+            int cnt = 0;
+
+            while(i < j) {
+                if(arr[i] + arr[j] == M) {
+                    i++;
                     j--;
+                    cnt++;
                 }
                 else {
-                    i++;
+                    if(arr[i] + arr[j] > M) {
+                        j--;
+                    }
+                    else {
+                        i++;
+                    }
                 }
             }
+
+            bw.write(String.valueOf(cnt));
+
+            br.close();
+            bw.close();
         }
-
-        bw.write(String.valueOf(cnt));
-
-        br.close();
-        bw.close();
     }
+
+
 }
